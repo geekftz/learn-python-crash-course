208d657376e0cc9336345122a62700e5c0b87f4e
fangxiang <fangxiang@immotors.com>
2024-03-01 15:16:08 +0800

    feat: add

--- 14/button.py
+++ 14/button.py
@@ -22,13 +22,14 @@ class Button():
 
     def prep_msg(self, msg):
         """render msg to image, and center it on the button"""
-        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
+        self.msg_image = self.font.render(msg, True, self.text_color,
+                                          self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
 
     def draw_button(self):
         # draw a button filled by color, then draw the text
         self.screen.fill(self.button_color, self.rect)
-        self.screen.blit(self.msg_image, self)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
 
 
