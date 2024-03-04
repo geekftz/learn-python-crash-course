93ccccc10bffdafad5aaf68822d34e84aaec6751
fangxiang <fangxiang@immotors.com>
2024-03-04 18:06:24 +0800

    feat: add

--- 15/15.3/rw_visual.py
+++ 15/15.3/rw_visual.py
@@ -9,28 +9,26 @@ from random_walk import RandomWalk
 # plt.scatter(rw.x_values, rw.y_values, s=15)
 # plt.show()
 
-
 #######################
 
 while True:
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+                edgecolor='none', s=15)
+
+    # highlight the starting point and ending point
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # hide coordinate axis
+    # plt.axes().get_xaxis().set_visible(False)
+    # plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("make another walk? (y/n): ")
     if keep_running == "n":
         break
-
-
-
-
-
-
-
-
-
-
-
-
-
