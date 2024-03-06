930ea3caf878e2f2d3b72c25e4f57425d5312e3b
fangxiang <fangxiang@immotors.com>
2024-03-06 17:23:25 +0800

    feat: add

--- 17/17.2/python_repos.py
+++ 17/17.2/python_repos.py
@@ -12,10 +12,18 @@ response_dict = r.json()
 print("Total repositories:", response_dict['total_count'])
 # 研究有关仓库的信息
 repo_dicts = response_dict['items']
-names, stars = [], []
+# names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    # stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
 
 # 可视化
 my_style = LS('#333366', base_style=LCS)
@@ -35,5 +43,6 @@ chart = pygal.Bar(my_config, style=my_style)
 
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
-chart.add('', stars)
-chart.render_to_file('python_repos3.svg')
+# chart.add('', stars)
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos5.svg')
