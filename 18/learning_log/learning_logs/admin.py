a5934371d32cafbd0e22fcbec99b5634a199d5ca
fangxiang <fangxiang@immotors.com>
2024-03-11 10:15:34 +0800

    feat: add

--- 18/learning_log/learning_logs/admin.py
+++ 18/learning_log/learning_logs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from learning_logs.models import Topic
+
 # Register your models here.
+
+admin.site.register(Topic)
--- 18/learning_log/learning_logs/models.py
+++ 18/learning_log/learning_logs/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
 class Topic(models.Model):
-    """theme that users learn"""
+    """the theme that userd learned"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """return the string representation of the model"""
+        """return string representation of models"""
         return self.text
+
+
+class Entry(models.Model):
+    """specific knowledge learned about a certain topic """
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """return string presentation of model"""
+        return self.text[:50] + '...'
