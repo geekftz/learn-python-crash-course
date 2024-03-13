19307eff7bb57e327548f5b5d85e04256b238c1f
fangxiang <fangxiang@immotors.com>
2024-03-13 11:16:28 +0800

    feat: add

--- 18/learning_log/learning_logs/urls.py
+++ 18/learning_log/learning_logs/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # main page
-    # re_path(r'^$', views.index, name='index'),
     path('', views.index, name='index'),
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name="topic")
 ]
--- 18/learning_log/learning_logs/views.py
+++ 18/learning_log/learning_logs/views.py
@@ -15,3 +15,11 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """show single topic and its details"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
