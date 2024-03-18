9256c85d9d94a9b22346436f3339b3e226596491
fangxiang <fangxiang@immotors.com>
2024-03-18 11:15:43 +0800

    feat: add

--- 18/learning_log/users/urls.py
+++ 18/learning_log/users/urls.py
@@ -3,10 +3,13 @@ from django.urls import path, include
 from django.contrib.auth import login
 # from django.contrib.auth.views import login
 
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
     # 登录页面
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login')
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
     # path('login/', LoginView.as_view(template_name='learning_logs/base.html'), name="login")
 ]
